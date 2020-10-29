ad88bba66b0bac00ad9f57a4bdab2bba2d79cf07
Gerardo <gahernandez02@gmail.com>
2020-10-29 15:05:51 -0700

    Added ImageFilterEditor project file

---
+++ ImageFilterEditor/ImageFilterEditor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageFilterEditor
+//
+//  Created by Gerardo Hernandez on 10/29/20.
+//  Copyright © 2020 Gerardo Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
